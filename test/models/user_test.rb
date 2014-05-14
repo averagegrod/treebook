2ef45c20c8a72c6076330b73ed765382400c8dee
Garrett Rodriguez <eloso79@gmail.com>
2014-05-14 15:53:36 -0700

    Add tests for user first name, last name, and profile

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,38 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "a user should enter a first name" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:first_name].empty?
+  end
+
+  test "a user should enter a last name" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:last_name].empty?
+  end
+
+  test "a user should enter a profile name" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  end
+
+  test "a user should have a unique profile_name" do	
+  	user = User.new
+  	user.profile_name = users(:garrett).profile_name
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  end
+
+    test "a user" do	
+  	user = User.new
+  	user.profile_name = "My Profile with spaces"
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  	assert user.errors[:profile_name]
+  end
+
 end
